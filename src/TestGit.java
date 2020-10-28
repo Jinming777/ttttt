bad60700581601fd6d58b0e1c9fe68775cf21043
Jinming777 <jinming_wang2012@163.com>
2020-10-28 10:22:54 +0800

    mygit库 dev c&p ming 202010281022

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,5 +1,7 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("ttttt库pull下TestGit后也进行了修改");
+
+        System.out.println("mygit库dev修改后提交，造成与master不一致后，去master创建新的分支");
     }
 }
