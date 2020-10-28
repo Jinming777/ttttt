8036ddbc41d56bdf9fb3f751641a43a52ad46367
Jinming777 <jinming_wang2012@163.com>
2020-10-28 10:34:32 +0800

    mygit库 hotfix c ming202010281034

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,5 +1,7 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("ttttt库pull下TestGit后也进行了修改");
+
+        System.out.println("mygit库基于master创建新的临时分支hotfix，修改TestGit后commit并合并到master");
     }
 }
